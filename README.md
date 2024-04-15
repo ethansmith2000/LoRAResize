16029e1a9f2cbe8371f903a2d65e7045d95dd961
ethansmith2000 <ethansmith2000@gmail.com>
2024-04-15 10:44:29 -0400

    add svd and clean up

--- README.md
+++ README.md
@@ -4,8 +4,11 @@ LoRAResize is a simple tool that allows you to further compress or expand a trai
 Expansion is useful if you've trained a LoRA then realized you'd like to try a larger rank but don't want to start training over
 Compression, I believe speaks for itself.
 
-There are two methods employed here, one uses a least squares optimization to estimate your equivalent lora at a different dimension.
-Alternatively, the simple_lora_expand function will just add rows/columns of zeros, however I'm not sure how this will affect training initializing the matrix with zeros, so I would proceed with caution.
+There are a few methods employed here:
+- "zero_pad": simply add rows/columns of zeros to the lora matrix, only for expansion, I'm not sure how this will affect training initializing the matrix with zeros, so I would proceed with caution.
+- "svd": fuses the lora components into a full size matrix then uses the singular value decomposition to get back components, although quite speedy at normal dimensions, if using for expansion I would also proceed with caution because expanded dimensions come out to be very small numbers near zero.
+- "optimization": randomly initialize the lora components of the desired rank, then uses gradient descent to optimize minimum reconstruction error with the full size matrix.
+- "auto": chooses between svd and optimization methods based on device and matrix dimensions, tuned for best possible speed
 
 A couple settings to be aware of when using the gradient optimization method:
 - down_name/up_name: the naming convention used for the lora layers in your model
@@ -13,11 +16,12 @@ A couple settings to be aware of when using the gradient optimization method:
 - start_lr/end_lr: a linearly interpolated learning rate used for the optimization
 - error_threshold: if any elements of the matrix exhibit a % error above this value, retry
 - allowed_num_tries: the number of times to retry the optimization before giving up
+- method: read above
 
 you may need to adjust the settings a bit to your needs
 
 ```python
-from lora_resize import change_lora_rank, simple_expand_lora
+from lora_resize import change_lora_rank
 import torch
 
 state_dict = torch.load("path/to/your/lora.pth")
--- lora_resize.py
+++ lora_resize.py
@@ -69,8 +69,10 @@ def get_least_squares_solution(weight_matrix, orig_up, orig_down, **kwargs):
             max_diff = diff.max().item()
             max_rel_error = (max_diff / matrix.flatten()[pos].item() + 1e-6) * 100
 
-        print(f"LOSS: {losses[-1]}, ABS MEAN {prediction.abs().mean():.7f} MAX DIFF: {max_diff:.7f}, MAX REL ERROR: {max_rel_error:.7f}%")
-        print(f"UP NORM {up.norm().item():.7f}, TARGET UP NORM {target_up_norm.item():.7f}, DOWN NORM {down.norm().item():.7f}, TARGET DOWN NORM {target_down_norm.item():.7f}")
+        print(
+            f"LOSS: {losses[-1]}, ABS MEAN {prediction.abs().mean():.7f} MAX DIFF: {max_diff:.7f}, MAX REL ERROR: {max_rel_error:.7f}%")
+        print(
+            f"UP NORM {up.norm().item():.7f}, TARGET UP NORM {target_up_norm.item():.7f}, DOWN NORM {down.norm().item():.7f}, TARGET DOWN NORM {target_down_norm.item():.7f}")
         if abs(max_rel_error) < error_threshold:
             finished = True
         else:
@@ -83,10 +85,41 @@ def get_least_squares_solution(weight_matrix, orig_up, orig_down, **kwargs):
     return up.detach().cpu(), down.detach().cpu().T
 
 
+def svd(full_mat, lora_up, lora_down, rank, **kwargs):
+    device = kwargs.get("device")
+    with torch.no_grad():
+        full_mat = full_mat.to(device).float()
+        u, s, v = torch.linalg.svd(full_mat)
+        min_dim = min(u.shape[1], v.shape[0])
+        s = s[:min_dim]
+        u = u[:, :min_dim] @ (s ** 0.5).diag()
+        v = (s ** 0.5).diag() @ v[:min_dim, :]
+
+        new_lora_up = u[:, :rank]
+        new_lora_down = v[:rank, :]
+
+        diff = torch.abs(new_lora_up @ new_lora_down - full_mat)
+        pos = diff.argmax()
+        max_diff = diff.max().item()
+        max_rel_error = (max_diff / full_mat.flatten()[pos].item() + 1e-6) * 100
+        print(f"MAX DIFF: {max_diff:.7f}, MAX REL ERROR: {max_rel_error:.7f}%")
+
+    return new_lora_up.cpu(), new_lora_down.cpu()
+
+
+def simple_expand(full_mat, lora_up, lora_down, rank, **kwargs):
+    new_lora_up = torch.zeros((lora_up.shape[0], rank), device=lora_up.device)
+    new_lora_down = torch.zeros((rank, lora_down.shape[1]), device=lora_down.device)
+    new_lora_up[:, :rank] = lora_up
+    new_lora_down[:rank, :] = lora_down
+
+    return new_lora_up, new_lora_down
+
+
 def change_lora_rank(state_dict,
                      rank=64,
                      down_name="lora_down",
-                    up_name="lora_up",
+                     up_name="lora_up",
                      equal_norm_penalty=0.0,  # 0.0001,
                      match_norm_penalty=0.0001,  # 0.0001,
                      steps=200,
@@ -100,8 +133,8 @@ def change_lora_rank(state_dict,
                      l1_factor=0.0,
                      error_threshold=10,
                      allowed_num_tries=20,
+                     method="optimization"  # ["auto", "svd", "optimization", "zero_pad"]
                      ):
-
     """
     :param state_dict: a torch state_dict
     :param rank: desired rank
@@ -120,13 +153,27 @@ def change_lora_rank(state_dict,
     :param l1_factor: l1 factor for mixed loss
     :param error_threshold: error threshold in percent, if the worst observed error is > error_threshold, retry
     :param allowed_num_tries: number of retries before raising an error
+    :param method: method to use, auto will choose between svd and optimization based on the size of the matrices
     :return: updated state_dict
     """
 
     lora_keys = [k for k in state_dict.keys() if down_name in k]
     weights_with_lora = [k.split(".lora")[0] for k in lora_keys]
-
     new_state_dict = {k: v for k, v in state_dict.items() if "lora" in k}
+    cur_rank = state_dict[lora_keys[0]].shape[0]
+
+    kwargs = dict(
+        rank=rank, steps=steps,
+        start_lr=start_lr, end_lr=end_lr,
+        device=device, match_norm_penalty=match_norm_penalty,
+        beta1=beta1, beta2=beta2,
+        weight_decay=weight_decay,
+        l=l,
+        l1_factor=l1_factor,
+        error_threshold=error_threshold,
+        equal_norm_penalty=equal_norm_penalty,
+        allowed_num_tries=allowed_num_tries,
+    )
 
     for key in tqdm(weights_with_lora):
         with torch.no_grad():
@@ -141,54 +188,18 @@ def change_lora_rank(state_dict,
             full_mat = lora_up @ lora_down
             print(key, full_mat.shape)
 
-        new_lora_up, new_lora_down = get_least_squares_solution(full_mat, lora_up, lora_down, rank=rank, steps=steps,
-                                                                start_lr=start_lr, end_lr=end_lr,
-                                                                device=device, match_norm_penalty=match_norm_penalty,
-                                                                beta1=beta1, beta2=beta2,
-                                                                weight_decay=weight_decay,
-                                                                l=l,
-                                                                l1_factor=l1_factor,
-                                                                error_threshold=error_threshold,
-                                                                equal_norm_penalty=equal_norm_penalty,
-                                                                allowed_num_tries=allowed_num_tries,
-                                                                )
-
-        if "conv" in key:
-            new_lora_up = new_lora_up[:, :, None, None]
-            new_lora_down = new_lora_down.reshape(new_lora_down.shape[0], new_lora_down.shape[1] // int(k * k), k, k)
-
-        new_state_dict[lora_down_key] = new_lora_down
-        new_state_dict[lora_up_key] = new_lora_up
-
-    state_dict.update(new_state_dict)
-
-    return state_dict
-
-
-def simple_expand_lora(state_dict, new_rank, down_name="lora_down", up_name="lora_up",):
-    lora_keys = [k for k in state_dict.keys() if down_name in k]
-    weights_with_lora = [k.split(".lora")[0] for k in lora_keys]
-    new_state_dict = {k: v for k, v in state_dict.items() if "lora" in k}
-
-    rank = state_dict[lora_keys[0]].shape[0]
-    assert new_rank > rank, "New rank must be greater than the old rank"
-
-    for key in tqdm(weights_with_lora):
-        with torch.no_grad():
-            lora_down_key, lora_up_key = key + f".{down_name}.weight", key + f".{up_name}.weight"
-            lora_down, lora_up = state_dict[lora_down_key], state_dict[lora_up_key]
-            if "conv" in key:
-                # out, rank, 1, 1
-                lora_up = lora_up.reshape(lora_up.shape[0], -1)
-                # rank, in, k, k
-                k = lora_down.shape[-1]
-                lora_down = lora_down.reshape(lora_down.shape[0], -1)
-            print(key)
-
-        new_lora_up = torch.zeros((lora_up.shape[0], new_rank), device=lora_up.device)
-        new_lora_down = torch.zeros((new_rank, lora_down.shape[1]), device=lora_down.device)
-        new_lora_up[:, :rank] = lora_up
-        new_lora_down[:rank, :] = lora_down
+        if method == "optimization":
+            new_lora_up, new_lora_down = get_least_squares_solution(full_mat, lora_up, lora_down, **kwargs)
+        elif method == "svd":
+            new_lora_up, new_lora_down = svd(full_mat, lora_up, lora_down, **kwargs)
+        elif method == "zero_pad":
+            new_lora_up, new_lora_down = simple_expand(full_mat, lora_up, lora_down, **kwargs)
+        elif method == "auto":
+            if (not "cuda" in device and max([*lora_down.shape, *lora_up.shape]) <= 1024) or max(
+                    [*lora_down.shape, *lora_up.shape]) <= 3072:
+                new_lora_up, new_lora_down = svd(full_mat, lora_up, lora_down, **kwargs)
+            else:
+                new_lora_up, new_lora_down = get_least_squares_solution(full_mat, lora_up, lora_down, **kwargs)
 
         if "conv" in key:
             new_lora_up = new_lora_up[:, :, None, None]
